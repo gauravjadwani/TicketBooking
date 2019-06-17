9918d8be7f2f200443921b9953f4e615ba4b4e09
Cosmonaut <gaurav.jadwani@hotmail.com>
2019-06-17 06:05:02 +0530

    Revert "Dev"

--- src/Utilities/HelperFunctions.js
+++
@@ -1,8 +0,0 @@
-// export default const getCircularReplacer = () => {
-export const charFrequency = arr => {
-  let count = {};
-  arr.forEach(function(s) {
-    count[s] ? count[s]++ : (count[s] = 1);
-  });
-  return count;
-};
--- src/Utilities/Queue.test.js
+++
@@ -1,4 +0,0 @@
-import Queue from "./Queue";
-
-let q = new Queue();
-q.createQueue({ id: 1, l: "temp" });
